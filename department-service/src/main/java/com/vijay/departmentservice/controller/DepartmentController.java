2d945e6c1b5dcecfe551039ba451415baeaa2010
Vijay Kumar <vijaykumarcseiiit@gmail.com>
2021-12-28 17:59:52 +0530

    chore : Logs added

--- department-service/src/main/java/com/vijay/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/vijay/departmentservice/controller/DepartmentController.java
@@ -3,11 +3,13 @@ package com.vijay.departmentservice.controller;
 import com.vijay.departmentservice.model.Department;
 import com.vijay.departmentservice.service.DepartmentService;
 import com.vijay.departmentservice.service.SequenceGeneratorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/department")
 public class DepartmentController {
@@ -26,12 +28,14 @@ public class DepartmentController {
 
     @PostMapping(path = "/save")
     public Department saveDepartment(@RequestBody Department department) {
+        log.info("save controller called in department");
         department.setDeptId(sequenceGeneratorService.generateSequence(Department.SEQUENCE_NAME));
         return departmentService.saveDepartment(department);
     }
 
     @GetMapping(path = "/departments/{id}")
     public Department getDepartmentById(@PathVariable long id){
+        log.info("get department by id is called from department");
         return departmentService.findByDepartmentByDepId(id);
     }
 
--- department-service/src/main/java/com/vijay/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/vijay/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -3,12 +3,14 @@ package com.vijay.departmentservice.service.impl;
 import com.vijay.departmentservice.model.Department;
 import com.vijay.departmentservice.repository.DepartmentRepository;
 import com.vijay.departmentservice.service.DepartmentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class DepartmentServiceImpl implements DepartmentService {
 
@@ -17,16 +19,19 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Override
     public Department saveDepartment(Department department) {
+        log.info("save method called in department from department");
         return departmentRepository.saveDeprtment(department);
     }
 
     @Override
     public Department findByDepartmentByDepId (long deptId) {
+        log.info("find by department id is called from department");
         return departmentRepository.getDepartmentById(deptId);
     }
 
     @Override
     public List<Department> getAllDepartments() {
+
         return departmentRepository.getAllDepartments();
     }
 }
